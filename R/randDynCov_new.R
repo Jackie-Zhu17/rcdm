c7751a76567a5dcf9bc1752887e0a106181f89cb
Hengcheng Zhu <zhu00578@umn.edu>
2022-11-29 11:52:08 -0600

    add R source files

--- R/randDynCov_new.R
+++ R/randDynCov_new.R
@@ -1,21 +1,21 @@
-#' Random Covariance Model
+#' Random Covariance Dynamic Model
 #'
-#' This function implements the Random Covariance Model (RCM) for joint estimation of
-#' multiple sparse precision matrices. Optimization is conducted using block
+#' This function implements the Random Covariance Dynamic Model (RCDM) for joint estimation of
+#' sparse time-varying precision matrices. Optimization is conducted using block
 #' coordinate descent.
 #' @param x data matrix of dimension \eqn{n} x \eqn{p}.
 #' @param h Bandwidth of the kernel smooth function.
 #' @param K Number of the knots.
 #' @param lambda1 Non-negative scalar. Induces sparsity in dynamic precision matrix.
 #' @param lambda2 Non-negative scalar. Induces similarity between dynamic precision matrix and kernel matrices.
-#' @param n0 Number of points in each segmentation.
+#' @param n0 Number of points in each segmentation(bin).
 #' @return A list of length 2 containing:
 #' \enumerate{
 #' \item Group-level precision matrix estimate (Omega0).
 #' \item \eqn{p} x \eqn{p} x \eqn{K} array of \eqn{K} subject-level precision matrix estimates (Omegas).
 #' }
 #' @author
-#' Lin Zhang, Hengcheng Zhu
+#' Hengcheng Zhu, Lin Zhang
 #'
 #' @export
 #'
@@ -26,44 +26,18 @@ library(doParallel)
 library(abind)
 library(plyr)
 acomb <- function(...) abind(..., along=3)
-# x_raw <- read.table('C:/Users/ZHC/Desktop/Lin/HCP_data/noGSR/1sess/ROI_TimeSeries/ROI_FCMap_100307_reordered_38.txt')[1:300,]
-# #x <- x_raw
-# x <- scale(x_raw)
-# library(loggle)
-# data(example)
-# X <- example$X
-# #load('./code_check/data_check.rda')
-# #X <- result$X
-# x <- t(X)
-# #
-# n0 <- 10
-# K <- 20
-# h = 31
-# rho <- 6e-06
-# lambda2 = 500
-# lambda1 <- (1+lambda2)*rho
-#lambda1 <- 0.0035
-randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list) {
+
+randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list, parallel=FALSE) {
 
   n = dim(x)[1]
   p = dim(x)[2]
-  
-  # segmentation
+
+  # bin segmentation
   T1 <- n - n %% n0
   Time_series_array <- sapply(1:(T1/n0), function(t) {x[seq(n0*t-n0+1,n0*t),]}, simplify = 'array')
   t_list <- seq(n0%/%2+1,T1,by=n0)
   T_count <- length(t_list)
-  
-  # S <- sapply(alply(Time_series_array,3), function(Tt) {cov(Tt)}, simplify = 'array')
-  # S <- sapply(alply(Time_series_array,3),
-  #              function(Tt) {t(scale(Tt, scale=FALSE)) %*% (scale(Tt, scale=FALSE))/(n0-1)},
-  #              simplify = 'array')
-  # S <- sapply(alply(Time_series_array,3),
-  #              function(Tt) {t(scale(Tt, scale=FALSE)) %*% (scale(Tt, scale=FALSE))/n0},
-  #              simplify = 'array')
-  # S <- sapply(alply(Time_series_array,3),
-  #              function(Tt) {t(Tt) %*% Tt/(n0-1)},
-  #              simplify = 'array')
+
   S <- sapply(alply(Time_series_array,3),
               function(Tt) {t(Tt) %*% Tt/n0},
               simplify = 'array')
@@ -76,10 +50,10 @@ randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list
   pos_k <- sapply(1:K,
                 function(k) {ifelse(k_list[k]%%n0==0, k_list[k]%/%n0, k_list[k]%/%n0+1)})
   S_k <- S[,,pos_k]
-  
+
   # criteria for convergence
   delta = 0.0001
-  
+
 
   # kernel smooth weight
   # dmat: K x T_count
@@ -88,7 +62,7 @@ randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list
   w_h_k_t <- apply(dmat, 2, function(dmat_t){dnorm(dmat_t/h)})
   colsum_w = colSums(w_h_k_t)
   rowsum_w = rowSums(w_h_k_t)
-  
+
 
   # inital values
   #Omega_k_list = sapply(alply(S_k,3), function(x1) solve(x1+diag(1,p)*0.01), simplify = 'array' )
@@ -100,163 +74,94 @@ randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list
     Omega_k_list[,,k] <- solve(St_wt + diag(1,p)*0.01)
   }
   Omega_t_list = sapply(alply(S,3), function(x1) solve(x1+diag(1,p)*0.01), simplify = 'array')
-  
-  
+
+
   Omega_k_list.old = array(0,c(p,p,K))
   Omega_t_list.old = array(0,c(p,p,T_count))
   count = 0
   state = 'Less than max.iter!'
-  
-  
-  # myCluster <- makeCluster(ncore, # number of cores to use
-  #                          type = "PSOCK") # type of cluster
-  # registerDoParallel(myCluster)
-  # 
-  # start BCD algorithm
-  
-  # pnll_list = c()
-  # nll_list = c()
-  # l1_list = c()
-  # KL_list = c()
-  while(max(abs(Omega_k_list-Omega_k_list.old))>delta | max(abs(Omega_t_list-Omega_t_list.old))>delta) {
-    # record current Omega0 & Omegas
-    Omega_k_list.old = Omega_k_list
-    Omega_t_list.old = Omega_t_list
-    
-    # 1st step :
-    for(t in 1:T_count) {
-      tmp <- apply(sapply(1:K, function(k){w_h_k_t[k,t]*solve(Omega_k_list[,,k])}, simplify = 'array'),
-                   1:2,
-                   sum)
-      st = (S[,,t]+lambda2*tmp)/(1+lambda2*colsum_w[t])
-      Omega_t_list[,,t] = glassoFast::glassoFast(st, rho)$wi
-      #Omega_t_list[,,t] = glassoFast::glassoFast(st, lambda1)$wi
-    }
-    
-    # 1st step(parallel) :
-    # Omega_t_list = foreach(t = 1:T_count, .combine='acomb', .multicombine=TRUE) %dopar% {
-    #   tmp <- apply(sapply(1:K, function(k){w_h_k_t[k,t]*solve(Omega_k_list[,,k])}, simplify = 'array'),
-    #                1:2,
-    #                sum)
-    #   st = (S[,,t] + lambda2*tmp)/(1+lambda2)
-    #   Omega_t = glassoFast::glassoFast(st, rho)$wi
-    #   #Omega_t = glasso::glasso(st, rho)$wi
-    #   return(Omega_t)
-    # }
-    
-    # 2nd step:
-    for(k in 1:K){
-      tmp <- apply(sapply(1:T_count, function(t){w_h_k_t[k,t]*Omega_t_list[,,t]}, simplify = 'array'),
-                   1:2,
-                   sum)
-      Omega_k_list[,,k] <- tmp/(rowsum_w[k])
-    }
 
-    # record BCD iterations
-    count = count+1
-    # cat('iteration',count,'done \n')
-    #print(count)
-    
-    # # record pnll,nll,l1,KL
-    # pnll = 0
-    # nll = 0
-    # l1 = 0
-    # KL = 0
-    # for (t in 1:T_count) {
-    #   tmp <- sapply(1:K, function(k){w_h_k_t[k,t]*(
-    #                                                 -log(det(Omega_t_list[,,t] %*% solve(Omega_k_list[,,k])))+
-    #                                                  sum(diag(Omega_t_list[,,t] %*% solve(Omega_k_list[,,k])))-
-    #                                                  p
-    #                                                )}, simplify = 'array')
-    #   pnll_t = sum(diag(Omega_t_list[,,t] %*% S[,,t])) - log(det(Omega_t_list[,,t])) +
-    #     lambda1 * sum(abs(Omega_t_list[,,t])) +
-    #     lambda2 * sum(tmp)
-    #   pnll = pnll + pnll_t
-    #   
-    #   nll_t = sum(diag(Omega_t_list[,,t] %*% S[,,t])) - log(det(Omega_t_list[,,t]))
-    #   nll = nll + nll_t
-    #   
-    #   l1_t = lambda1 * sum(abs(Omega_t_list[,,t]))
-    #   l1 = l1 + l1_t
-    #   
-    #   KL_t = lambda2 * sum(tmp)
-    #   KL = KL + KL_t
-    # }
-    # pnll_list = c(pnll_list, pnll)
-    # nll_list = c(nll_list, nll)
-    # l1_list = c(l1_list, l1)
-    # KL_list = c(KL_list, KL)
-    # cat('pnll',pnll,'\n')
-    # cat('nll',nll,'\n')
-    # cat('l1',l1,'\n')
-    # cat('KL',KL,'\n')
-    
-    if (count == 100) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
+  if (parallel == TRUE) {
+    myCluster <- makeCluster(ncore, # number of cores to use
+                             type = "PSOCK") # type of cluster
+    registerDoParallel(myCluster)
+
+    # start BCD algorithm
+    while(max(abs(Omega_k_list-Omega_k_list.old))>delta | max(abs(Omega_t_list-Omega_t_list.old))>delta) {
+      # record current Omega0 & Omegas
+      Omega_k_list.old = Omega_k_list
+      Omega_t_list.old = Omega_t_list
+
+      # 1st step(parallel) :
+      Omega_t_list = foreach(t = 1:T_count, .combine='acomb', .multicombine=TRUE) %dopar% {
+        tmp <- apply(sapply(1:K, function(k){w_h_k_t[k,t]*solve(Omega_k_list[,,k])}, simplify = 'array'),
+                     1:2,
+                     sum)
+        st = (S[,,t] + lambda2*tmp)/(1+lambda2)
+        Omega_t = glassoFast::glassoFast(st, rho)$wi
+        return(Omega_t)
+      }
+
+      # 2nd step:
+      for(k in 1:K){
+        tmp <- apply(sapply(1:T_count, function(t){w_h_k_t[k,t]*Omega_t_list[,,t]}, simplify = 'array'),
+                     1:2,
+                     sum)
+        Omega_k_list[,,k] <- tmp/(rowsum_w[k])
+      }
+
+      # record BCD iterations
+      count = count+1
+      # cat('iteration',count,'done \n')
+
+      if(count>max.iter) {
+        #cat('Omegas fail to converge. \n')
+        state = 'Exceed max.iter!'
         break
       }
     }
-    
-    if (count == 500) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
+    stopCluster(myCluster)
+
+  } else {
+    # start BCD algorithm
+    while(max(abs(Omega_k_list-Omega_k_list.old))>delta | max(abs(Omega_t_list-Omega_t_list.old))>delta) {
+      # record current Omega0 & Omegas
+      Omega_k_list.old = Omega_k_list
+      Omega_t_list.old = Omega_t_list
+
+      # 1st step :
+      for(t in 1:T_count) {
+        tmp <- apply(sapply(1:K, function(k){w_h_k_t[k,t]*solve(Omega_k_list[,,k])}, simplify = 'array'),
+                     1:2,
+                     sum)
+        st = (S[,,t]+lambda2*tmp)/(1+lambda2*colsum_w[t])
+        Omega_t_list[,,t] = glassoFast::glassoFast(st, rho)$wi
+      }
+
+      # 2nd step:
+      for(k in 1:K){
+        tmp <- apply(sapply(1:T_count, function(t){w_h_k_t[k,t]*Omega_t_list[,,t]}, simplify = 'array'),
+                     1:2,
+                     sum)
+        Omega_k_list[,,k] <- tmp/(rowsum_w[k])
+      }
+
+      # record BCD iterations
+      count = count+1
+      # cat('iteration',count,'done \n')
+
+      if(count>max.iter) {
+        #cat('Omegas fail to converge. \n')
+        state = 'Exceed max.iter!'
         break
       }
-    }
-    
-    if (count == 1000) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
-        break
-      }
-    }
-    
-    if (count == 2000) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
-        break
-      }
-    }
-    
-    if(count>max.iter) {
-      #cat('Omegas fail to converge. \n')
-      state = 'Exceed max.iter!'
-      break
     }
   }
-  
-  #stopCluster(myCluster)
-  
+
+
   res = list(t_list, Omega_t_list, S, state, count)
   names(res)=c("t_list", "Omega_t_list", "S", "state", "count")
-  
+
   return(res)
 }
-
-# omega_t_1 <- Omega_t_list[,,1]
-# omega_true_8 <- as.matrix(result$Omega.t.true[,,8])
-# 
-# Omega_t_true <- array(NA, c(15,15,1001))
-# for (i in 1:1001){
-#   Omega_t_true[,,i] <- as.matrix(example$Omega.true[[i]])
-# }
-# 
-# tmp <- Omega_t_list[,,3]
-# Omega.true_tmp <- Omega_t_true[,,36]
-# 
-# 
-
-# edge.num.opt <- sapply(1:T_count, function(i) (sum(round(Omega_t_list[,,i], 3)!=0)-p)/2)
-# edge.num.true <- sapply(1:T_count, function(i) (sum(round(Omega_t_true[,,t_list[i]], 3)!=0)-p)/2)
-# edge.num.overlap <- sapply(1:T_count, function(i)
-#   (sum(round(Omega_t_list[,,i], 3) & round(Omega_t_true[,,t_list[i]], 3))-p)/2)
-# perform.matrix <- cbind("FDR" = 1 - edge.num.overlap / edge.num.opt,
-#                         "power/TPR" = edge.num.overlap / edge.num.true,
-#                         "FPR" = (edge.num.opt-edge.num.overlap)/(p*(p-1)/2-edge.num.true))
-# print(apply(perform.matrix, 2, mean))
 
--- R/randDynCov_new_cluster.R
+++ R/randDynCov_new_cluster.R
@@ -1,7 +1,7 @@
-#' Random Covariance Model
+#' Random Covariance Dynamic Model(cluster)
 #'
-#' This function implements the Random Covariance Model (RCM) for joint estimation of
-#' multiple sparse precision matrices. Optimization is conducted using block
+#' This function implements the Random Covariance Dynamic Model (RCDM) for joint estimation of
+#' sparse time-varying precision matrices. Optimization is conducted using block
 #' coordinate descent.
 #' @param x data matrix of dimension \eqn{n} x \eqn{p}.
 #' @param h Bandwidth of the kernel smooth function.
@@ -15,7 +15,7 @@
 #' \item \eqn{p} x \eqn{p} x \eqn{K} array of \eqn{K} subject-level precision matrix estimates (Omegas).
 #' }
 #' @author
-#' Lin Zhang, Hengcheng Zhu
+#' Hengcheng Zhu, Lin Zhang.
 #'
 #' @export
 #'
@@ -26,18 +26,18 @@ library(doParallel)
 library(abind)
 library(plyr)
 acomb <- function(...) abind(..., along=3)
-randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list=NULL, G) {
+randDynCov_cluster <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list=NULL, G, parallel=FALSE) {
 
   n = dim(x)[1]
   p = dim(x)[2]
-  
-  
+
+
   # segmentation
   T1 <- n - n %% n0
   Time_series_array <- sapply(1:(T1/n0), function(t) {x[seq(n0*t-n0+1,n0*t),]}, simplify = 'array')
   t_list <- seq(n0%/%2+1,T1,by=n0)
   T_count <- length(t_list)
-  
+
   S <- sapply(alply(Time_series_array,3),
               function(Tt) {t(Tt) %*% Tt/n0},
               simplify = 'array')
@@ -50,16 +50,16 @@ randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list
   pos_k <- sapply(1:K,
                 function(k) {ifelse(k_list[k]%%n0==0, k_list[k]%/%n0, k_list[k]%/%n0+1)})
   S_k <- S[,,pos_k]
-  
+
   # cluster list
   cluster_list = list()
   for (g in 1:G) {
     cluster_list[[g]] = seq(g, g+G*(K/G-1), by = G)
   }
-  
+
   # criteria for convergence
   delta = 0.0001
-  
+
 
   # kernel smooth weight
   # dmat: K x T_count
@@ -68,7 +68,7 @@ randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list
   w_h_k_t <- apply(dmat, 2, function(dmat_t){dnorm(dmat_t/h)})
   colsum_w = colSums(w_h_k_t)
   rowsum_w = rowSums(w_h_k_t)
-  
+
 
   # inital values
   Omega_k_list <- array(NA, c(p,p,K))
@@ -79,92 +79,98 @@ randDynCov <- function(x, h, K, max.iter=1000, rho, lambda2, n0, ncore=8, k_list
     Omega_k_list[,,k] <- solve(St_wt + diag(1,p)*0.01)
   }
   Omega_t_list = sapply(alply(S,3), function(x1) solve(x1+diag(1,p)*0.01), simplify = 'array')
-  
-  
+
+
   Omega_k_list.old = array(0,c(p,p,K))
   Omega_t_list.old = array(0,c(p,p,T_count))
   count = 0
   state = 'Less than max.iter!'
-  
-  
-  # start BCD algorithm
-  
-  while(max(abs(Omega_k_list-Omega_k_list.old))>delta | max(abs(Omega_t_list-Omega_t_list.old))>delta) {
-    # record current Omega0 & Omegas
-    Omega_k_list.old = Omega_k_list
-    Omega_t_list.old = Omega_t_list
-    
-    # 1st step :
-    for(t in 1:T_count) {
-      tmp <- apply(sapply(1:K, function(k){w_h_k_t[k,t]*solve(Omega_k_list[,,k])}, simplify = 'array'),
-                   1:2,
-                   sum)
-      st = (S[,,t] + lambda2*tmp)/(1+lambda2*colsum_w[t])
-      Omega_t_list[,,t] = glassoFast::glassoFast(st, rho)$wi
-      #Omega_t_list[,,t] = glassoFast::glassoFast(st, lambda1)$wi
-    }
-    
-    
-    # 2nd step:
-    for (g in 1:G) {
-      tmp = 0
-      for (k in cluster_list[[g]]) {
-        tmp = tmp + apply(sapply(1:T_count, function(t){w_h_k_t[k,t]*Omega_t_list[,,t]}, simplify = 'array'),
-                          1:2,
-                          sum)
-      }
-      Omega_k_list[,,cluster_list[[g]]] = tmp/sum(rowsum_w[cluster_list[[g]]])
-    }
 
-    # record BCD iterations
-    count = count+1
-    # cat('iteration',count,'done \n')
-    # print(count)
-    
-    if (count == 100) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
+  if (parallel == TRUE) {
+    myCluster <- makeCluster(ncore, # number of cores to use
+                             type = "PSOCK") # type of cluster
+    registerDoParallel(myCluster)
+    # start BCD algorithm
+
+    while(max(abs(Omega_k_list-Omega_k_list.old))>delta | max(abs(Omega_t_list-Omega_t_list.old))>delta) {
+      # record current Omega0 & Omegas
+      Omega_k_list.old = Omega_k_list
+      Omega_t_list.old = Omega_t_list
+
+      # 1st step(parallel) :
+      Omega_t_list = foreach(t = 1:T_count, .combine='acomb', .multicombine=TRUE) %dopar% {
+        tmp <- apply(sapply(1:K, function(k){w_h_k_t[k,t]*solve(Omega_k_list[,,k])}, simplify = 'array'),
+                     1:2,
+                     sum)
+        st = (S[,,t] + lambda2*tmp)/(1+lambda2*colsum_w[t])
+        Omega_t = glassoFast::glassoFast(st, rho)$wi
+        return(Omega_t)
+      }
+
+      # 2nd step:
+      for (g in 1:G) {
+        tmp = 0
+        for (k in cluster_list[[g]]) {
+          tmp = tmp + apply(sapply(1:T_count, function(t){w_h_k_t[k,t]*Omega_t_list[,,t]}, simplify = 'array'),
+                            1:2,
+                            sum)
+        }
+        Omega_k_list[,,cluster_list[[g]]] = tmp/sum(rowsum_w[cluster_list[[g]]])
+      }
+
+      # record BCD iterations
+      count = count+1
+      # cat('iteration',count,'done \n')
+
+      if(count>max.iter) {
+        #cat('Omegas fail to converge. \n')
+        state = 'Exceed max.iter!'
         break
       }
     }
-    
-    if (count == 500) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
+    stopCluster(myCluster)
+  } else {
+    # start BCD algorithm
+    while(max(abs(Omega_k_list-Omega_k_list.old))>delta | max(abs(Omega_t_list-Omega_t_list.old))>delta) {
+      # record current Omega0 & Omegas
+      Omega_k_list.old = Omega_k_list
+      Omega_t_list.old = Omega_t_list
+
+      # 1st step :
+      for(t in 1:T_count) {
+        tmp <- apply(sapply(1:K, function(k){w_h_k_t[k,t]*solve(Omega_k_list[,,k])}, simplify = 'array'),
+                     1:2,
+                     sum)
+        st = (S[,,t]+lambda2*tmp)/(1+lambda2*colsum_w[t])
+        Omega_t_list[,,t] = glassoFast::glassoFast(st, rho)$wi
+      }
+
+      # 2nd step:
+      for (g in 1:G) {
+        tmp = 0
+        for (k in cluster_list[[g]]) {
+          tmp = tmp + apply(sapply(1:T_count, function(t){w_h_k_t[k,t]*Omega_t_list[,,t]}, simplify = 'array'),
+                            1:2,
+                            sum)
+        }
+        Omega_k_list[,,cluster_list[[g]]] = tmp/sum(rowsum_w[cluster_list[[g]]])
+      }
+
+      # record BCD iterations
+      count = count+1
+      # cat('iteration',count,'done \n')
+
+      if(count>max.iter) {
+        #cat('Omegas fail to converge. \n')
+        state = 'Exceed max.iter!'
         break
       }
-    }
-    
-    if (count == 1000) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
-        break
-      }
-    }
-    
-    if (count == 2000) {
-      if (sum(round(Omega_t_list[,,1],3) != 0) - p == 0){
-        #cat('Too sparse! \n')
-        state = 'Too sparse, break!'
-        break
-      }
-    }
-    
-    if(count>max.iter) {
-      #cat('Omegas fail to converge. \n')
-      state = 'Exceed max.iter!'
-      break
     }
   }
-  
-  #stopCluster(myCluster)
-  
+
   res = list(t_list, Omega_t_list, S, state, count)
   names(res)=c("t_list", "Omega_t_list", "S", "state", "count")
-  
+
   return(res)
 }
 
